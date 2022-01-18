cbd39b557646c77a0817d60bf8b47b68a305dea2
Sahana20081 <94735338+Sahana20081@users.noreply.github.com>
2022-01-18 12:52:53 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+var box1;
+
+function setup() 
+{
+  createCanvas(400, 400);
+  box1=new Box();
+  box2=new Box();
+}
+
+function draw() 
+{
+  background(220);
+  box1.show();
+  box1.set_speed(1);
+  box2.show();
+  box2.set_speed(2);
+  box2.set_width(20);
+
+}
+
